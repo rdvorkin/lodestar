a76e5b363785df999559637031136b9b75b17150
matthewkeil <me@matthewkeil.com>
2023-08-01 02:37:33 -0600

    fix(metrics): add network worker unit name back

--- packages/beacon-node/src/util/workerEvents.ts
+++ packages/beacon-node/src/util/workerEvents.ts
@@ -40,7 +40,7 @@ export function wireEventsOnWorkerThread<EventData>(
     ) {
       const [sec, nanoSec] = process.hrtime(data.posted);
       const networkWorkerLatency = sec + nanoSec / 1e9;
-      metrics?.networkWorkerWireEventsOnWorkerThreadLatency.observe(
+      metrics?.networkWorkerWireEventsOnWorkerThreadLatencySec.observe(
         {eventName: data.event as string},
         networkWorkerLatency
       );
@@ -81,7 +81,7 @@ export function wireEventsOnMainThread<EventData>(
     ) {
       const [sec, nanoSec] = process.hrtime(data.posted);
       const networkWorkerLatency = sec + nanoSec / 1e9;
-      metrics?.networkWorkerWireEventsOnMainThreadLatency.observe(
+      metrics?.networkWorkerWireEventsOnMainThreadLatencySec.observe(
         {eventName: data.event as string},
         networkWorkerLatency
       );
